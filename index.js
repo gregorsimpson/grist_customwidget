ffe274959637641d21dfe29736b444d74d699128
Brot <gregorsimpson20@gmail.com>
2024-01-06 00:01:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,20 +6,13 @@
   }
 }
 
-var data = {
-  message: '',
-  tester: 'TESTER!'
-}
-
 function handleError(err) {
   console.error('ERROR', err);
   data.message = String(err).replace(/^Error: /, '');
 }
 
 ready(async function() {
-  Vue.config.errorHandler = handleError;
-  app = new Vue({
-    el: '#app',
-    data: data
-  });
+  let html = '<h1>jao.</h1><u>check me out</u><script type="text/javascript">console.log("EPIC!");</script>';
+  let range = document.createRange();
+  let fragment = range.createContextualFragment(html);
 });
